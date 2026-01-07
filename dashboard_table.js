7fe00e4b08870be257d3bc51e812e9a14a61cd0b
tajahshelley <tajah.shelley@reprise.com>
2026-01-07 18:17:18 -0500

    add

---
+++ dashboard_table.js
@@ -0,0 +1,429 @@
+////////////////////////////////////////
+// Dashboard Table Update
+////////////////////////////////////////
+
+const policy_obj = {
+    "policy_id": "face9662-178d-40fd-b7f1-aafdaa053604",
+    "policy": {
+        "id": {
+            "value": "face9662-178d-40fd-b7f1-aafdaa053604"
+        },
+        "applicant_id": {
+            "value": "c61b19dc-67b5-4b74-9e78-aaddf0364b84"
+        },
+        "insured_id": {
+            "value": "c61b19dc-67b5-4b74-9e78-aaddf0364b84"
+        },
+        "owner_id": {
+            "value": "c61b19dc-67b5-4b74-9e78-aaddf0364b84"
+        },
+        "payor_id": {
+            "value": "c61b19dc-67b5-4b74-9e78-aaddf0364b84"
+        },
+        "payor_type": "NATURAL_CUSTOMER",
+        "writing_agent_code": "",
+        "sales_channel": "",
+        "sales_mode": "SALES_MODE_AGENT_LED",
+        "agent_sales_channel": "AA",
+        "carrier": "TLIC",
+        "type": "WHOLE_LIFE",
+        "source": "IP",
+        "organization_code": "ORG_TRANSAMERICA",
+        "application_date": "2025-09-18T18:47:35.000Z",
+        "application_start_date": "2025-09-18T18:46:13.000Z",
+        "insured_age_at_submit": 0,
+        "application_id": "8c3a8f86-78ec-4a18-8901-2c05b93886f4",
+        "owner_relationship": "UNKNOWN",
+        "bind_details": {
+            "billing_mode": "UNKNOWN_BILLINGMODE"
+        },
+        "approved_rates": [],
+        "offers": [{
+            "issue_age": 35,
+            "min_line": {
+                "cents": "500000"
+            },
+            "max_line": {
+                "cents": "5000000"
+            },
+            "starts": {
+                "year": 2025,
+                "month": 9,
+                "day": 18
+            },
+            "ends": {
+                "year": 2025,
+                "month": 11,
+                "day": 17
+            },
+            "products": [{
+                "code": "TAFEST01",
+                "riders": []
+            }],
+            "gender": "MALE",
+            "flat_extras": []
+        }],
+        "policy_number": "FEXB658645",
+        "is_replacement": false,
+        "has_existing": false,
+        "policy_replacement_disclosure": {
+            "has_existing_insurance": false,
+            "is_replacement": false,
+            "total_existing_coverage": {
+                "cents": "0"
+            },
+            "replacement_reason": "",
+            "existing_funds": false,
+            "read_aloud": false,
+            "policy_replacements_comparison_info": false,
+            "replacing_policies": [],
+            "existing_policies": [],
+            "replacement_company": "",
+            "replacement_original_owner": false,
+            "non_compliance_explanation": "",
+            "has_complied": false
+        },
+        "reinsurance": false,
+        "reinsurance_company": "",
+        "duration_years": 0,
+        "face_amount": {
+            "cents": "3182700"
+        },
+        "risk_class": "SELECT_NON_TOBACCO",
+        "risk_class_aud_text": "",
+        "risk_class_descriptor": {
+            "rule_class_name": "select_nt",
+            "rule_class_label": "Select NT",
+            "rank": 1,
+            "type": "STANDARD",
+            "tobacco": false,
+            "offer_class_name": "select_non_tobacco",
+            "offer_class_filed_label": "Select Non-Tobacco",
+            "offer_class_ui_label": "Select Non-Tobacco",
+            "metadata": {}
+        },
+        "aud_required": false,
+        "product_line": {
+            "code": "TLIC_FEE",
+            "version": "1.1"
+        },
+        "product_code": "TAFEST01",
+        "pricing_version": "1.0.2",
+        "issue_state": "TX",
+        "issue_country": "US",
+        "stripe_customer_id": "",
+        "stripe_subscription_id": "",
+        "platform_customer_id": "",
+        "monthaversary": 0,
+        "insured_issue_age": 0,
+        "billing_mode": "UNKNOWN_BILLINGMODE",
+        "billing_group_id": "",
+        "is_mail_sent": false,
+        "is_substandard_mail_sent": false,
+        "policy_status": "APPROVED",
+        "policy_substatus": "ACTIVE",
+        "policy_composed_status": "APPROVED_ACTIVE",
+        "policy_status_last_changed": "2025-09-18T18:47:59.568Z",
+        "do_not_reinstate": false,
+        "do_not_reinstate_reason": "",
+        "flags": [],
+        "billing_subscription_id": {
+            "value": "43720279-62c6-461e-936f-d2e78b34733f"
+        },
+        "illustration_id": "",
+        "first_year_commission_rate": 0,
+        "renewal_year_commission_rate": 0,
+        "commission_rates": [],
+        "commissions": [],
+        "policy_rate": 0,
+        "cancellation_reason": "UNKNOWN_REASON",
+        "cancellation_reason_other": "",
+        "primary_beneficiaries": [],
+        "contingent_beneficiaries": [],
+        "documents": {
+            "SUPPLEMENTAL_AGENT_REPORT": "18032414-93da-40cd-be24-2e3881af87a4",
+            "E_DELIVERY_TERMS_AND_CONDITIONS_OF_USE": "2edb9ae8-9aed-48cb-8ecc-681ebbb70c27",
+            "DISCLOSURE_MIB": "f4ed4b7a-7fc3-469c-93df-4f84d3dfa9b1",
+            "PRIVACY_NOTICE": "673bf2e0-6ec0-4e2a-b592-24f436251d0a",
+            "EVEREST_RIDER_CONSENT": "de2f6972-af3d-4e2c-a0f9-40616bb6aa0a",
+            "ACCELERATED_DEATH_BENEFIT_RIDER_DISCLOSURE": "6df5d875-b824-4a33-8b86-e233ff952bb9",
+            "SIGNED_APP": "1d976958-8733-470e-bc50-a7ccbdad72f4",
+            "DISCLOSURE_HIPAA": "753f2a58-1f23-416a-a9c5-846b9cfee0e7",
+            "ELECTRONIC_OPT_IN": "20f400bf-b18d-4c1a-9946-107b8a2483eb"
+        },
+        "replacements": [],
+        "stale_date": "2025-11-17T00:00:00.000Z",
+        "grace_lapse_reasons": [],
+        "face_amount_changes": [],
+        "grace_period_changed_reason": "UNKNOWN",
+        "created_date": "2025-09-18T18:47:59.568Z",
+        "updated_date": "2025-09-18T18:48:48.299Z",
+        "version": "3",
+        "deleted": false,
+        "initial_expiration_notification_sent": false,
+        "followup_expiration_notification_sent": false,
+        "underwriting_method": "UM_INSTANT",
+        "apl_enabled": false,
+        "labels": [],
+        "riders_added": [{
+            "product_code": "TARDAS01",
+            "added": true
+        }],
+        "riders": [{
+            "product_code": "TARDAS01",
+            "added": true,
+            "selected": false
+        }],
+        "checkout_state": "CS_READY",
+        "flat_extras": [],
+        "external_contract_id": "",
+        "additional_application_data": [],
+        "policy_actors": [{
+            "id": {
+                "value": "c61b19dc-67b5-4b74-9e78-aaddf0364b84"
+            },
+            "role": "ROLE_INSURED",
+            "relationship_to_insured": "RT_SELF"
+        }, {
+            "id": {
+                "value": "c61b19dc-67b5-4b74-9e78-aaddf0364b84"
+            },
+            "role": "ROLE_OWNER",
+            "relationship_to_insured": "RT_SELF"
+        }, {
+            "id": {
+                "value": "c61b19dc-67b5-4b74-9e78-aaddf0364b84"
+            },
+            "role": "ROLE_PAYOR",
+            "relationship_to_insured": "RT_SELF"
+        }]
+    }
+};
+
+const product_code_map = {
+    "TAFEST01": "FE Express Solution℠",
+    "TAIUL01": "Transamerica Financial Foundation IUL® II"
+}
+
+const quote_coverage_map = {
+    "select_non_tobacco": "Select Non-Tobacco"
+}
+
+// function set_entry_in_table(root, parsed_data, {
+//     name,
+//     product_code,
+//     quoted_coverage_type,
+//     quoted_coverage_amt,
+//     approved_coverage_type,
+//     approved_coverage_amt,
+//     status,
+//     substatus,
+//     start_date
+// }) {
+
+//     // HTML Updates
+//     const table_row_elem = root.querySelector("tr.sc-csjKPv");
+//     const header_elem = table_row_elem.querySelector("h3.sc-kAyceB");
+//     const product_elem = table_row_elem.querySelector("p.sc-kAyceB.iOORVB");
+//     const quoted_coverage_elems = table_row_elem.querySelectorAll("div > p.sc-kAyceB.iOORVB");
+//     const quoted_coverage_type_elem = quoted_coverage_elems[0];
+//     const quoted_coverage_amt_elem = quoted_coverage_elems[1];
+//     const approved_coverage_elem = table_row_elem.querySelector("p.sc-kAyceB.fUZYwa");
+//     const start_date_elem = table_row_elem.querySelector("p.sc-kAyceB.fsbGvn");
+//     const status_elem = table_row_elem.querySelector("div.sc-dhKdcB.gDmEv");
+
+//     if (name) header_elem.textContent = name;
+//     if (product_code) product_elem.textContent = product_code_map[product_code];
+//     if (quoted_coverage_type) quoted_coverage_type_elem.textContent = quote_coverage_map[quoted_coverage_type];
+//     if (quoted_coverage_amt) quoted_coverage_amt_elem.textContent = quoted_coverage_amt;
+//     approved_coverage_elem.textContent = approved_coverage_type ? quote_coverage_map[approved_coverage_type] : "No Coverage Selected";
+//     if (start_date) start_date_elem.textContent = start_date;
+//     if (status) status_elem.textContent = status;
+
+//     // JSON Updates
+//     const applications = parsed_data.state.loaderData["routes/_main.$basePath.$"].viewContext.applications;
+
+//     if (name) {
+//         applications[0].applicant.name = name;
+//     }
+//     if (product_code) {
+//         applications[0].product.product_code = product_code;
+//     }
+//     if (quoted_coverage_type) {
+//         applications[0].quote.risk_class = quoted_coverage_type;
+//     }
+//     if (quoted_coverage_amt) {
+//         applications[0].quote.face_amount = parseFloat(quoted_coverage_amt.replace(/[$,]/g, '')).toFixed(2);
+//     }
+//     if (approved_coverage_type || approved_coverage_amt) {
+//         if (!applications[0].policy) {
+//             applications[0]["policy"] = policy_obj.policy;
+//             applications[0]["policy_id"] = policy_obj.policy_id;
+//         }
+//         if (approved_coverage_type) {
+//             applications[0].policy.risk_class = approved_coverage_type.toUpperCase();
+//         }
+//         if (approved_coverage_amt) {
+//             applications[0].policy.face_amount.cents = approved_coverage_amt.replace(/[$,]/g, '') * 100;
+//         }
+//     }
+//     if (start_date) {
+//         const [month, day, year] = start_date.split("/");
+//         console.log("month, day, year: ", month, day, year);
+//         applications[0].date_started_iso = `${year}-${month}-${day}T08:57:16`
+//     }
+//     if (status) {
+//         applications[0].application_status = status;
+//     }
+//     if (substatus) {
+//         applications[0].application_substatus = substatus;
+//     }
+
+//     parsed_data.state.loaderData["routes/_main.$basePath.$"].viewContext.applications = applications;
+//     return parsed_data;
+// }
+
+function set_entry_in_table_json(parsed_data, {
+    name,
+    product_code,
+    quoted_coverage_type,
+    quoted_coverage_amt,
+    approved_coverage_type,
+    approved_coverage_amt,
+    status,
+    substatus,
+    start_date
+}) {
+
+    // JSON Updates
+    const applications = parsed_data.viewContext.applications;
+
+    if (name) {
+        applications[0].applicant.name = name;
+    }
+    if (product_code) {
+        applications[0].product.product_code = product_code;
+    }
+    if (quoted_coverage_type) {
+        applications[0].quote.risk_class = quoted_coverage_type;
+    }
+    if (quoted_coverage_amt) {
+        applications[0].quote.face_amount = parseFloat(quoted_coverage_amt.replace(/[$,]/g, '')).toFixed(2);
+    }
+    if (approved_coverage_type || approved_coverage_amt) {
+        if (!applications[0].policy) {
+            applications[0]["policy"] = policy_obj.policy;
+            applications[0]["policy_id"] = policy_obj.policy_id;
+        }
+        if (approved_coverage_type) {
+            applications[0].policy.risk_class = approved_coverage_type.toUpperCase();
+        }
+        if (approved_coverage_amt) {
+            applications[0].policy.face_amount.cents = approved_coverage_amt.replace(/[$,]/g, '') * 100;
+        }
+    }
+    if (start_date) {
+        const [month, day, year] = start_date.split("/");
+        console.log("month, day, year: ", month, day, year);
+        applications[0].date_started_iso = `${year}-${month}-${day}T08:57:16`
+    }
+    if (status) {
+        applications[0].application_status = status;
+    }
+    if (substatus) {
+        applications[0].application_substatus = substatus;
+    }
+
+    parsed_data.viewContext.applications = applications;
+    return parsed_data;
+}
+
+// replay_backend.get("/agent/dashboard", (request) => {
+//     const url_obj = new URL(request.url);
+//     return url_obj.search === "";
+// }).post_process(async (resp) => {
+//     let html_resp = await resp.text();
+//     const root = replay_backend.html_parser.parse(html_resp);
+//     const app_info = await replay_backend.storage.get("app_info");
+//     if (app_info) {
+//         // Parse JSON data
+//         let json_code_text = root.querySelector("#root script:nth-of-type(8)").childNodes[0]._rawText;
+//         json_code_text = json_code_text.replace("window.__remixContext = ", "");
+//         let parsed_data = null;
+//         eval(`parsed_data = ${json_code_text}`);
+
+//         const name = app_info.viewContext.owner.first_name + " " + app_info.viewContext.owner.last_name;
+//         const type = app_info.viewContext.policy.riskClass.toLower();
+//         const product_code = app_info.viewContext.policy.products[0].productCode;
+//         const start_date_obj = app_info.viewContext.policy.offers[0].starts;
+//         const start_date = `${start_date_obj.month}/${start_date_obj.day}/${start_date_obj.year}`
+//         // Update table entry
+//         const updated_data = set_entry_in_table(root, parsed_data, {
+//             name,
+//             product_code, // or "TAIUL01"
+//             quoted_coverage_type: type,
+//             quoted_coverage_amt: "$87,765",
+//             approved_coverage_type: type,
+//             approved_coverage_amt: "$90,000",
+//             status: "submitted",
+//             substatus: null,
+//             start_date,
+//         });
+
+//         // Serialize JSON back into string
+//         const json_string_modified = `window.__remixContext = ${JSON.stringify(updated_data)};`;
+//         root.querySelector("#root script:nth-of-type(8)").textContent = json_string_modified;
+//     }
+
+//     return root.toString();
+// });
+
+//////////////////////////////////////////////
+// Dashboard Table Update After Completion
+/////////////////////////////////////////////
+
+replay_backend.get("/agent/dashboard", (request, body) => {
+    const formData = Object.fromEntries(
+        new URLSearchParams(body)
+    );
+    const filter = (
+        new URL(request.url).searchParams.get("_data") ===
+        "routes/_main.$basePath.$"
+    ) && formData.interactionId !== "sync-agent";
+    console.log("request filter: ", filter);
+    return filter;
+}).post_process(async (resp) => {
+    let json_resp = await resp.json();
+    const app_info = await replay_backend.storage.get("app_info");
+
+    if (app_info && json_resp.viewContext?.applications) {
+        const name = app_info.viewContext.owner.first_name + " " + app_info.viewContext.owner.last_name;
+        const type = app_info.viewContext.policy.riskClass.toLowerCase();
+        const product_code = app_info.viewContext.policy.offers[0].products[0].productCode;
+        const start_date_obj = new Date();
+        const month = start_date_obj.getUTCMonth() + 1; // months from 1-12
+        const day = start_date_obj.getUTCDate();
+        const year = start_date_obj.getUTCFullYear();
+
+        // Using padded values, so that 2023/1/7 becomes 2023/01/07
+        const pMonth = month.toString().padStart(2, "0");
+        const pDay = day.toString().padStart(2, "0");
+        const start_date = `${pMonth}/${pDay}/${year}`
+        // Update table entry through JSON
+        const updated_data = set_entry_in_table_json(json_resp, {
+            name,
+            product_code, // or "TAIUL01"
+            quoted_coverage_type: type,
+            quoted_coverage_amt: "$87,765",
+            approved_coverage_type: type,
+            approved_coverage_amt: "$90,000",
+            status: "submitted",
+            substatus: null,
+            start_date,
+        });
+        console.log("Updated data: ", updated_data);
+        json_resp = updated_data;
+    }
+
+    return JSON.stringify(json_resp);
+});
